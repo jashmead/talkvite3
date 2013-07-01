08e534373c4e9cdebc4df358c77ebd6826d17655
jashmead <john.ashmead@ashmeadsoftware.com>
2013-07-01 19:20:11 -0400

    added trickery to make sure correct rake & rails run

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Talk Vite'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Talk Vite')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Talk Vite | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Talk Vite | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Talk Vite | About")
+    end
+  end
+
+end
