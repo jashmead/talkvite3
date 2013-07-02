6e9c0543895d9725c7c38f44df9f497c48523866
jashmead <john.ashmead@ashmeadsoftware.com>
2013-07-02 16:00:01 -0400

    have added contact page, routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,44 +1,62 @@
 require 'spec_helper'
 
-describe "Static Pages" do
+describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Talk Vite'" do
-      visit '/static_pages/home'
+    it "should have the h1 'Talk Vite'" do
+      visit root_path
       expect(page).to have_content('Talk Vite')
     end
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
+    it "should have the base title" do
+      visit root_path
       expect(page).to have_title("Talk Vite")
+    end
+
+    it "should not have a custom page title" do
+      visit root_path
+      expect(page).not_to have_title('Home')
     end
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
+    it "should have the h1 'Help'" do
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Talk Vite Help")
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+    it "should have the h1 'About'" do
+      visit about_path
       expect(page).to have_content('About')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Talk Vite About")
     end
   end
 
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Talk Vite Contact")
+    end
+  end
 end
+
