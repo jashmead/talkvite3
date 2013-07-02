aa6676d461f3c0f3310f2d58a58e6b6d3964d4aa
jashmead <john.ashmead@ashmeadsoftware.com>
2013-07-02 16:39:44 -0400

    Have simplified rspec, added routes, started sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Talkvite3::Application.routes.draw do
 
+  get "users/new"
+
   # static pages:
   root to: 'static_pages#home'
-
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
